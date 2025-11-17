60471bd8efaac2918f3e1004cdb5fa309307b825
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-11-17 21:02:54 +0600

    problem solve no 3

--- solution.ts
+++ solution.ts
@@ -22,3 +22,19 @@ function getLength(value: string | any[]) {
   }
 }
 //3
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+//4
