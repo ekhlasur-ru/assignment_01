13283b2d7478c4c095121650f1fb26a470f088b9
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-11-17 19:34:49 +0600

    problem 01 solve

---
+++ solution.ts
@@ -0,0 +1,13 @@
+function formatValue(value: string | number | boolean) {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  }
+  if (typeof value === "number") {
+    return value * 10;
+  }
+  if (typeof value === "boolean") {
+    return !value;
+  }
+
+  return value;
+}
